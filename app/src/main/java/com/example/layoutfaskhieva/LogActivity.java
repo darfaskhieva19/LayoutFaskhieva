2fb2086618f22ba8556aa46192ec5d8b270af005
daria_faskhieva <dashaf712@mail.ru>
2022-11-16 13:57:56 +0300

    commitD

--- app/src/main/java/com/example/layoutfaskhieva/LogActivity.java
+++ app/src/main/java/com/example/layoutfaskhieva/LogActivity.java
@@ -2,13 +2,29 @@ package com.example.layoutfaskhieva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class LogActivity extends AppCompatActivity {
+
+    private CheckBox checkBox;
+    Button reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        checkBox = findViewById(R.id.chkBox);
+        reg = findViewById(R.id.btnLog);
+    }
+
+    public void onRegister(View view) {
+        Intent regIntent = new Intent(this,RegActivity.class);
+        startActivity(regIntent);
     }
 }
--- app/src/main/java/com/example/layoutfaskhieva/MainActivity.java
+++ app/src/main/java/com/example/layoutfaskhieva/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.layoutfaskhieva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final int SPLASH_DISPLAY_LENGHT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this,LogActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
     }
 }
